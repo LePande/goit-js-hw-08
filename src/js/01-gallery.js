ea183219780af7ebc1a4b1cce230c8dafdcd14f2
LePande <119473273+LePande@users.noreply.github.com>
2023-09-18 22:27:31 -0500

    Tarea Video Finalizada

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const selectGallery = document.querySelector('ul.gallery');
+selectGallery.classList.add('gallery');
+
+galleryItems.forEach(function (galleryItem) {
+  const createLi = document.createElement('li');
+  createLi.classList.add('gallery__item');
+  selectGallery.appendChild(createLi);
+
+  const createBtn = document.createElement('a');
+  createLi.append(createBtn);
+  createBtn.classList.add('gallery__link');
+  createBtn.href = galleryItem.original;
+
+  const createImg = document.createElement('img');
+  createBtn.append(createImg);
+  createImg.classList.add('gallery__image');
+  createImg.src = galleryItem.preview;
+  createImg.dataset.source = galleryItem.original;
+  createImg.alt = galleryItem.description;
+
+  createBtn.addEventListener('click', e => {
+    e.preventDefault();
+  });
+});
+
+let gallery = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const updatePlaybackTime = throttle(function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', updatePlaybackTime);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+  player
+    .setCurrentTime(parseFloat(savedTime))
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
